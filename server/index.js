0a759d2c3f992e978bfef75c1a989ab95e007589
Rsjii <rsji1806@gmail.com>
2024-07-23 23:56:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { SocketProvider } from "./context/SocketProvider";
+import "client/src/service/index.css";
+import App from "client/src/service/App";
+import reportWebVitals from "client/src/service/reportWebVitals";
+import { SocketProvider } from "client/src/context/SocketProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
